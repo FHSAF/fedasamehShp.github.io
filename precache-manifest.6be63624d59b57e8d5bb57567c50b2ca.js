a875acce388e5779b9e566181cbcbe5fd18efbf0
myGit@git.com <myGit@git.com>
2019-12-06 19:52:38 +0600

    Updates

---
+++ precache-manifest.6be63624d59b57e8d5bb57567c50b2ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a8ca73b684fce4088735b07db3713a",
+    "url": "/shop-hub2/index.html"
+  },
+  {
+    "revision": "26f1f5862fb1bc2e7fc2",
+    "url": "/shop-hub2/static/css/main.053be60a.chunk.css"
+  },
+  {
+    "revision": "0cfff17e4924b449d85f",
+    "url": "/shop-hub2/static/js/2.7cc6c66f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shop-hub2/static/js/2.7cc6c66f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26f1f5862fb1bc2e7fc2",
+    "url": "/shop-hub2/static/js/main.84f12a0d.chunk.js"
+  },
+  {
+    "revision": "08ba1b56ab6f52d08773",
+    "url": "/shop-hub2/static/js/runtime-main.fb4bd158.js"
+  }
+]);
