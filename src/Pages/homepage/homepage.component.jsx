a2ea15eb59de7fadaf6581f03e41ec5fa95f2e22
myGit@git.com <myGit@git.com>
2019-12-06 19:50:11 +0600

    first commit part 4

---
+++ src/Pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './hompage.styles.scss';
+import Directory from '../../Components/directory/directory.component'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory/>
+    </div>
+)
+export default HomePage
