4495fe0ff641d6ae851c60d5597bc7ca4b381f0c
myGit@git.com <myGit@git.com>
2019-12-08 02:53:13 +0600

    adding header adn styling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './Pages/homepage/homepage.component';
 import ShopPage from './Pages/shop/shop.component.jsx';
+import Header from './Components/header/header.component.jsx'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
